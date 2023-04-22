86ef1699b10695730ecf8cacfdcc8f43626329b6
m4naal <130424262+m4naal@users.noreply.github.com>
2023-04-21 21:47:46 -0500

    idk hw10?

--- homework/urls.py
+++ homework/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path("student", include('students.urls')),
     path('admin/', admin.site.urls),
 ]
+
