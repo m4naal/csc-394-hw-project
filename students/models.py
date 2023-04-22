6c2e5a5ad06234598e1cb1cfcf19a4015cf9d9ce
m4naal <130424262+m4naal@users.noreply.github.com>
2023-04-21 20:42:06 -0500

    half of 9

--- students/models.py
+++ students/models.py
@@ -7,3 +7,9 @@ class Student(models.Model):
     last_name = models.CharField(max_length=30)
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
+class Course(models.Model):
+    department = models.CharField(max_length=60)
+    number = models.CharField(max_length=3)
+    students = models.ManyToManyField(Student)
+    def __str__(self) -> str:
+        return self.department + "-" + self.number
